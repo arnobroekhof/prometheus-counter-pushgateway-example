ce1897b5cdb70c90386e3394e96175f300c39adf
Arno Broekhof <arnobroekhof@gmail.com>
2021-10-20 09:22:40 +0200

    Added push count

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from prometheus_client import CollectorRegistry, Counter, push_to_gateway
 
 def main():
     gateway = os.getenv('PUSH_GATEWAY_URI', 'prometheus-pushgateway.prometheus:9091')
+    push_count = os.getenv('PUSH_COUNT', 10)
     registry = CollectorRegistry()
     counter = Counter('example_counter_pushed_cronjob_processed_total', 'Total processed by the push gateway',
                       registry=registry)
-    counter.inc(10)
+    counter.inc(push_count)
     push_to_gateway(gateway, job='prometheus-counter-pushgateway-example', registry=registry)
 
 
