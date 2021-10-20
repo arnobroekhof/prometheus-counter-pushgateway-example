7257459f8c770ea5f5fd0ddf3417571ec147e4dd
Arno Broekhof <arnobroekhof@gmail.com>
2021-10-20 09:26:26 +0200

    Changed counter

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from prometheus_client import CollectorRegistry, Counter, push_to_gateway
 def main():
     gateway = os.getenv('PUSH_GATEWAY_URI', 'prometheus-pushgateway.prometheus:9091')
     push_count = os.getenv('PUSH_COUNT', 10)
+    print(f'Using counter of {push_count}')
     registry = CollectorRegistry()
     counter = Counter('example_counter_pushed_cronjob_processed_total', 'Total processed by the push gateway',
                       registry=registry)
